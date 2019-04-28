33b5f698b7f15832527f1fc2bac524702527612a
mcverter <mitchell.verter@gmail.com>
2019-04-28 12:34:28 -0700

    changing table structure

---
+++ db/CreateTables.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS facilities CASCADE;
+CREATE TABLE "facilities" (
+  id INTEGER PRIMARY KEY,
+  facilityName TEXT,
+  city TEXT,
+  state CHARACTER(2),
+  UNIQUE (id, facilityName, city, state)
+) ;
+
+DROP TABLE IF EXISTS "inmates" CASCADE;
+CREATE TABLE inmates (
+    firstname TEXT,
+    lastname TEXT,
+    dob DATE,
+    facilityId INTEGER REFERENCES facilities(id),
+    entered DATE,
+    exited DATE,
+    UNIQUE (firstname, lastname, dob, entered, exited, facilityId)
+);
