e71e9b76c929f5e3fb6188ea0dad37e1fed017fa
mcverter <mitchell.verter@gmail.com>
2019-05-01 19:52:57 -0700

    adding to server

--- server/app.js
+++ server/app.js
@@ -1,18 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+const cors = require('cors')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let indexRouter = require('./routes/index');
+let facilitiesRouter = require('./routes/facilities');
+let inmatesRouter = require('./routes/inmates');
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,7 +23,37 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/api/facilities", facilitiesRouter);
+app.use("/api/inmates", inmatesRouter);
+
+
+/*
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/web/public')));
+
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  let sentFilePath = req.url.startsWith("index.html") ?
+    "/index.html" : req.url;
+  res.sendFile(path.join(__dirname,  '/../client/web/build', sentFilePath));
+});
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ server/gsnipe-server-app.js
@@ -0,0 +1,53 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require('cors')
+
+const indexRouter = require("./routes/index");
+const kropotkinRouter = require("./routes/kropotkins");
+const shareableRouter = require("./routes/shareables");
+
+const app = express();
+
+app.use(cors())
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/api/shareable", shareableRouter);
+app.use("/api/kropotkin", kropotkinRouter);
+app.use("/api/shareables", shareableRouter);
+app.use("/api/kropotkins", kropotkinRouter);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/web/public')));
+
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  let sentFilePath = req.url.startsWith("index.html") ?
+    "/index.html" : req.url;
+res.sendFile(path.join(__dirname,  '/../client/web/build', sentFilePath));
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
